df38b8507e5645a27f4befe328d97ac0020452a4
PuffinDev <puffin.develop@gmail.com>
2022-05-14 18:07:30 +0100

    Gui tweaks

--- gui.py
+++ gui.py
@@ -11,8 +11,8 @@ groups = {
 
 # weights change the chances of a letter being picked.
 weights = {
-    "V": [8, 11, 4, 8, 5, 5],
-    "C": [11, 4, 4, 3, 11, 3, 2, 10, 5, 2, 1, 5],
+    "V": [8, 9, 5, 8, 5, 5],
+    "C": [8, 4, 4, 3, 8, 3, 2, 6, 5, 2, 1, 5],
 }
 
 # replaces text in the generated words
@@ -37,7 +37,6 @@ def gen_words():
     t1.delete("1.0", END)
     t1.insert(END, pseudotext)
 
-
 root = Tk()
 root.geometry("500x600")
 root.resizable(False, False)
@@ -47,16 +46,19 @@ l1.pack(pady=(5, 10))
 
 l2 = Label(text="Pattern", font=("", 13))
 l2.pack()
-e1 = Entry(font=("", 13))
+
+pat = IntVar()
+e1 = Entry(font=("", 13), text=pat)
+pat.set("CV(CV)(C)/VC(VC)(V)")
 e1.pack(pady=(5, 10))
 
-s1 = Scale(orient=HORIZONTAL, from_=1, to=200)
+s1 = Scale(orient=HORIZONTAL, from_=1, to=150)
 s1.pack(pady=(5, 10))
 
 b1 = Button(text="Generate!", font=("", 13), command=gen_words)
 b1.pack(pady=(0, 20))
 
-t1 = Text(font=("", 14))
+t1 = Text(wrap=WORD, font=("", 14))
 t1.pack()
 
 
